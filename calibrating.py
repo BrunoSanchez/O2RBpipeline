6a76dfbe1052390e52e74c9459e7704d65b44436
Bruno SANCHEZ <bruno@oac.unc.edu.ar>
2018-05-16 16:00:58 -0300

    FInished phot calibration

---
+++ calibrating.py
@@ -0,0 +1,134 @@
+ipython phot_calibration.py PGC073926/pgc073926_170104.fits
+ipython phot_calibration.py PGC073926/pgc073926_170113_full.new
+ipython phot_calibration.py PGC073926/pgc073926_171116_b_median.new
+
+
+#  PGC147285
+ipython phot_calibration.py PGC147285/pgc147285_170104.fits
+ipython phot_calibration.py PGC147285/pgc147285_170114_1.fits
+ipython phot_calibration.py PGC147285/pgc147285_170114_2.fits
+ipython phot_calibration.py PGC147285/pgc147285_171116_3_median.fits
+
+#  ESO364-014
+ipython phot_calibration.py ESO364-014/eso364-014_170113_1.fits
+ipython phot_calibration.py ESO364-014/eso364-014_170113_2.fits
+ipython phot_calibration.py ESO364-014/eso364-014_170113_3.fits
+ipython phot_calibration.py ESO364-014/eso364-014_170113_4.fits
+ipython phot_calibration.py ESO364-014/eso364-014_170113_5.fits
+ipython phot_calibration.py ESO364-014/eso364-014_170113_6.fits
+ipython phot_calibration.py ESO364-014/eso364-014_170113_7.fits
+ipython phot_calibration.py ESO364-014/eso364-014_170113_8.fits
+ipython phot_calibration.py ESO364-014/eso364-014_170113_9.fits
+ipython phot_calibration.py ESO364-014/eso364-014_171116_1_median_w.fits
+ipython phot_calibration.py ESO364-014/eso364-014_171116_2_median_w.fits
+
+# ESO487-003
+ipython phot_calibration.py ESO487-003/eso487-003_170113_1_median_cropped.fits
+ipython phot_calibration.py ESO487-003/eso487-003_171118_1_median.fits
+ipython phot_calibration.py ESO487-003/eso487-003_171118_2_median.fits
+ipython phot_calibration.py ESO487-003/eso487-003_171118_b_median.fits
+
+
+#  NGC1341
+ipython phot_calibration.py NGC1341/ngc1341_170113.fits
+ipython phot_calibration.py NGC1341/ngc1341_170121.fits
+ipython phot_calibration.py NGC1341/ngc1341_171118_b_median_w.fits
+ipython phot_calibration.py NGC1341/ngc1341_171116_b_median.fits
+ipython phot_calibration.py NGC1341/ngc1341_171119_b_median_w.fits
+
+# NGC1808
+ipython phot_calibration.py NGC1808/ngc1808_170113_1.fits
+ipython phot_calibration.py ../20171118/NGC1808/calibrated/NGC1808-001_30seg_cal.fits
+ipython phot_calibration.py ../20171118/NGC1808/calibrated/NGC1808-010_30seg_cal.fits
+ipython phot_calibration.py ../20171118/NGC1808/calibrated/NGC1808-015_30seg_cal.fits
+ipython phot_calibration.py ../20171118/NGC1808/calibrated/NGC1808-020_30seg_cal.fits
+
+
+#  ESO202-009
+ipython phot_calibration.py ../20170114/eso202-009/calibrated/eso0202-009-016_cal.fits
+ipython phot_calibration.py ../20170114/eso202-009/calibrated/eso0202-009-018_cal.fits
+ipython phot_calibration.py ../20170114/eso202-009/calibrated/eso0202-009-027_cal.fits
+ipython phot_calibration.py ../20170114/eso202-009/calibrated/eso0202-009-036_cal.fits
+ipython phot_calibration.py ../20170114/eso202-009/calibrated/eso0202-009-045_cal.fits
+ipython phot_calibration.py ../20170114/eso202-009/calibrated/eso0202-009-054_cal.fits
+ipython phot_calibration.py ../20170114/eso202-009/calibrated/eso0202-009-060_cal.fits
+ipython phot_calibration.py ESO202-009/eso202-009_171116_b_median_w.fits
+
+# ESO242-018
+ipython phot_calibration.py ../20170115/eso242-018/calibrated/eso242-018-002_cal.fits
+ipython phot_calibration.py ESO242-018/eso242-018_171118_1_median.fits
+ipython phot_calibration.py ../20170115/eso242-018/calibrated/eso242-018-008_cal.fits
+ipython phot_calibration.py ../20170115/eso242-018/calibrated/eso242-018-012_cal.fits
+ipython phot_calibration.py ../20170115/eso242-018/calibrated/eso242-018-019_cal.fits
+ipython phot_calibration.py ../20170115/eso242-018/calibrated/eso242-018-024_cal.fits
+ipython phot_calibration.py ../20170115/eso242-018/calibrated/eso242-018-036_cal.fits
+ipython phot_calibration.py ../20170115/eso242-018/calibrated/eso242-018-042_cal.fits
+ipython phot_calibration.py ../20170115/eso242-018/calibrated/eso242-018-050_cal.fits
+
+
+# ESO555-022
+ipython phot_calibration.py ESO555-022/eso555-022_170115.fits
+ipython phot_calibration.py ESO555-022/eso555-022_170115_1.fits
+ipython phot_calibration.py ESO555-022/eso555-022_170115_2.fits
+ipython phot_calibration.py ESO555-022/eso555-022_171116_1_median.fits
+ipython phot_calibration.py ESO555-022/eso555-022_171116_2_median.fits
+ipython phot_calibration.py ESO555-022/eso555-022_171119_b_median_w.fits
+
+#  ESO425-010
+ipython phot_calibration.py../20170115/eso425-010/calibrated/eso425-010-001_cal.fits
+ipython phot_calibration.py../20170115/eso425-010/calibrated/eso425-010-015_cal.fits
+ipython phot_calibration.py../20170115/eso425-010/calibrated/eso425-010-030_cal.fits
+ipython phot_calibration.py../20170115/eso425-010/calibrated/eso425-010-044_cal.fits
+ipython phot_calibration.py../20171119/ESO425-010/calibrated/ESO425-010-000_30s_cal.fits
+ipython phot_calibration.py../20171119/ESO425-010/calibrated/ESO425-010-003_30s_cal.fits
+ipython phot_calibration.py../20171119/ESO425-010/calibrated/ESO425-010-006_30s_cal.fits
+ipython phot_calibration.py../20171119/ESO425-010/calibrated/ESO425-010-009_30s_cal.fits
+ipython phot_calibration.py../20171119/ESO425-010/calibrated/ESO425-010-012_30s_cal.fits
+
+# NGC1567
+ipython phot_calibration.py NGC1567/ngc1567_20170115_1.fits
+ipython phot_calibration.py NGC1567/ngc1567_20170115_2.fits
+ipython phot_calibration.py NGC1567/ngc1567_171116_1_median.fits
+ipython phot_calibration.py NGC1567/ngc1567_171116_2_median.fits
+ipython phot_calibration.py NGC1567/ngc1567_171116_3_median.fits
+ipython phot_calibration.py NGC1567/ngc1567_171116_4_median.fits
+ipython phot_calibration.py NGC1567/ngc1567_171116_5_median.fits
+ipython phot_calibration.py NGC1567/ngc1567_171116_6_median.fits
+
+# PGC3080859
+ipython phot_calibration.py PGC3080859/pgc3080859_171118_b_median_w.fits
+ipython phot_calibration.py ../20170115/pgc3080859/calibrated/pgc3080859-003_cal.fits
+ipython phot_calibration.py ../20170115/pgc3080859/calibrated/pgc3080859-009_cal.fits
+ipython phot_calibration.py ../20170115/pgc3080859/calibrated/pgc3080859-015_cal.fits
+ipython phot_calibration.py ../20170115/pgc3080859/calibrated/pgc3080859-020_cal.fits
+ipython phot_calibration.py ../20170115/pgc3080859/calibrated/pgc3080859-027_cal.fits
+ipython phot_calibration.py ../20170115/pgc3080859/calibrated/pgc3080859-034_cal.fits
+ipython phot_calibration.py ../20170115/pgc3080859/calibrated/pgc3080859-042_cal.fits
+
+# UGC04959
+ipython phot_calibration.py UGC04959/ugc04959_mask_mean_20171119.fits
+ipython phot_calibration.py  UGC04959/ugc04959_20170120.fits
+
+
+### Python terminal
+#~ import pandas as pd
+
+
+#~ dt = pd.read_table('calibrations.txt', delim_whitespace=True, index_col=False)
+#~ cal_res_inv = (1./dt.CAL_RES)
+
+#~ w = len(cal_res_inv)*cal_res_inv/np.sum(cal_res_inv)
+
+#~ def weighted_avg_and_std(values, weights):
+    #~ """
+    #~ Return the weighted average and standard deviation.
+
+    #~ values, weights -- Numpy ndarrays with the same shape.
+    #~ """
+    #~ average = np.average(values, weights=weights)
+    #~ # Fast and numerically precise:
+    #~ variance = np.average((values-average)**2, weights=weights)
+    #~ return (average, np.sqrt(variance))
+#~ weighted_avg_and_std(dt.max_mag[dt.CAL_BAND=='V'], weights=w[dt.CAL_BAND=='V'])
+#~ weighted_avg_and_std(dt.max_mag[dt.CAL_BAND=='R'], weights=w[dt.CAL_BAND=='R'])
+
--- phot_calibration.py
+++ phot_calibration.py
@@ -26,8 +26,9 @@ import os
 import numpy as np
 
 from astropy.io import fits
+from astropy.io import ascii
 
-from astropy import coordinates
+from astropy import coordinates as coord
 from astropy import units as u
 from astropy.wcs import WCS
 
@@ -55,89 +56,298 @@ from ps_query import panstarrs_query
 import matplotlib.pyplot as plt
 
 basepath = '/home/bruno/Data/LIGO_O2/Jan04/newstacks'
-image_path = os.path.join(basepath, 'PGC073926/pgc073926_170104.fits')
+#image_path = os.path.join(basepath, 'PGC073926/pgc073926_170104.fits')
+def cal_image(img_path):
+    image_path = os.path.join(basepath, img_path)
 
-# load img with WCS
-img = image_path.strip('.fits') + '.new'
+    # load img with WCS
+    if not '.new' in image_path:
+        img = image_path.strip('.fits') + '.new'
+    else:
+        img = image_path
 
-hdr = fits.getheader(img)
-data = fits.getdata(img)
-wcs = WCS(hdr)
+    fits_file = fits.open(img, mode='update')
+    hdr = fits_file[0].header #fits.getheader(img)
+    data = fits_file[0].data  # fits.getdata(img)
+    wcs = WCS(hdr)
 
-# Obtain image center
-crval_ra= hdr['CRVAL1']
-crval_dec = hdr['CRVAL2']
-crval_x = hdr['CRPIX1']
-crval_y = hdr['CRPIY1']
+    # Obtain image center
+    crval_ra= hdr['CRVAL1']
+    crval_dec = hdr['CRVAL2']
+    crval_x = hdr['CRPIX1']
+    crval_y = hdr['CRPIX2']
 
-# query PanSTARRS DR1
-ps1_tab = panstarrs_query(crval_ra, crval_dec, 0.5, maxsources=300)
+    # Get sources in image
+    mask = make_source_mask(data, snr=2, npixels=5, dilate_size=11)
+    mask_zeros = data==0
+    mask = mask | mask_zeros
 
-# Get sources in image
-mask = make_source_mask(data, snr=2, npixels=5, dilate_size=11)
-mask_zeros = data==0
-mask = mask | mask_zeros
+    mean, median, std = sigma_clipped_stats(data, sigma=3.0, mask=mask)
+    sigma_clip = SigmaClip(sigma=3., iters=10)
+    bkg_estimator = MedianBackground()
+    bkg_estimator = SExtractorBackground()
 
-mean, median, std = sigma_clipped_stats(data, sigma=3.0, mask=mask)
-sigma_clip = SigmaClip(sigma=3., iters=10)
-bkg_estimator = MedianBackground()
-bkg_estimator = SExtractorBackground()
+    bkg = Background2D(data, (50, 50), filter_size=(3, 3),
+                       sigma_clip=sigma_clip,
+                       bkg_estimator=bkg_estimator,
+                       mask=mask)
 
-bkg = Background2D(data, (50, 50), filter_size=(3, 3),
-                   sigma_clip=sigma_clip,
-                   bkg_estimator=bkg_estimator,
-                   mask=mask)
+    back = bkg.background * ~mask
 
-back = bkg.background * ~mask
+    daofind = DAOStarFinder(fwhm=5.0, threshold=5.*std)
+    sources = daofind(data - median)
+    #print(sources)
 
-daofind = DAOStarFinder(fwhm=5.0, threshold=8.*std)
-sources = daofind(data - median)
-print(sources)
+    positions = (sources['xcentroid'], sources['ycentroid'])
+    xypos = np.array([sources['xcentroid'], sources['ycentroid']]).T
+    sky_pos = wcs.all_pix2world(xypos, 0)
+    skycoords = coord.SkyCoord(sky_pos*u.deg, frame='icrs')
 
-positions = (sources['xcentroid'], sources['ycentroid'])
-xypos = np.array([sources['xcentroid'], sources['ycentroid']]).T
-sky_pos = wcs.all_pix2world(xypos, 0)
-skycoords = coordinates.SkyCoord(sky_pos*u.deg, frame='icrs')
+    radii = [5.*u.arcsec, 7.*u.arcsec, 10.*u.arcsec]
+    annulus = [(6.*u.arcsec, 8.*u.arcsec),
+               (9.*u.arcsec, 11.*u.arcsec),
+               (11.*u.arcsec, 12.*u.arcsec)]
 
-radii = [2.*u.arcsec, 3.*u.arcsec, 5.*u.arcsec]
-annulus = [(5.*u.arcsec, 7.*u.arcsec),
-           (6.*u.arcsec, 8.*u.arcsec),
-           (8.*u.arcsec, 10.*u.arcsec)]
+    apertures = [SkyCircularAperture(skycoords, r=r).to_pixel(wcs)
+                 for r in radii]
+    annulii = [SkyCircularAnnulus(skycoords, r_in=ri, r_out=ro).to_pixel(wcs)
+               for ri, ro in annulus]
+    apers = apertures + annulii
 
-apertures = [SkyCircularAperture(skycoords, r=r).to_pixel(wcs)
-             for r in radii]
-annulii = [SkyCircularAnnulus(skycoords, r_in=ri, r_out=ro).to_pixel(wcs)
-           for ri, ro in annulus]
-apers = [apertures, annulii]
-phot_table = aperture_photometry(data-back, apertures) # did not work annulus
+    #~ tables = [aperture_photometry(data-back, aper) for aper in apers]
+    phot_table = aperture_photometry(data-back, apers)
+
+    bkg_sum_0 = apertures[0].area()*phot_table['aperture_sum_3']/annulii[0].area()
+    bkg_sum_1 = apertures[0].area()*phot_table['aperture_sum_4']/annulii[1].area()
+    bkg_sum_2 = apertures[0].area()*phot_table['aperture_sum_5']/annulii[2].area()
+
+    final_sum_0 = phot_table['aperture_sum_0'] - bkg_sum_0
+    final_sum_1 = phot_table['aperture_sum_1'] - bkg_sum_1
+    final_sum_2 = phot_table['aperture_sum_2'] - bkg_sum_2
+
+    phot_table['resid_aper_sum_0'] = final_sum_0
+    phot_table['resid_aper_sum_1'] = final_sum_1
+    phot_table['resid_aper_sum_2'] = final_sum_2
+
+    phot_table['cmag_0'] = -2.5 * np.log10(final_sum_0)
+    phot_table['cmag_1'] = -2.5 * np.log10(final_sum_1)
+    phot_table['cmag_2'] = -2.5 * np.log10(final_sum_2)
+
+    phot_table['mag_0'] = -2.5 * np.log10(phot_table['aperture_sum_0'])
+    phot_table['mag_1'] = -2.5 * np.log10(phot_table['aperture_sum_1'])
+    phot_table['mag_2'] = -2.5 * np.log10(phot_table['aperture_sum_2'])
+
+    phot_table['RA'] = skycoords.ra
+    phot_table['Dec'] = skycoords.dec
+
+    # match the catalogs
+    # attempt a xmatch
+    phot_table.write('/tmp/sourcecat.csv', overwrite=True)
+    try:
+        from astroquery.xmatch import XMatch
+        XMatch.TIMEOUT = 600
+        try:
+            table = XMatch.query(cat1=open('/tmp/sourcecat.csv'),
+                             #cat2='vizier:II/336/apass9', #apass
+                             cat2='vizier:I/322A/out',   # UCAC4
+                             #cat2='vizier:II/246/out',   # 2MASS
+                             max_distance=5 * u.arcsec,
+                             colRA1='RA',
+                             colDec1='Dec',
+                             colRA2='RAJ2000',
+                             colDec2='DEJ2000')
+            print 'Using UCAC4'
+            print table.colnames
+            if 'Rmag' not in table.colnames and 'rmag' in table.colnames:
+                table['Rmag'] = table['rmag']
+        except:
+            try:
+                table = XMatch.query(cat1=open('/tmp/sourcecat.csv'),
+                                 cat2='vizier:II/336/apass9', #apass
+                                 #cat2='vizier:I/322A/out',   # UCAC4
+                                 #cat2='vizier:II/246/out',   # 2MASS
+                                 max_distance=5 * u.arcsec,
+                                 colRA1='RA',
+                                 colDec1='Dec',
+                                 colRA2='RAJ2000',
+                                 colDec2='DEJ2000')
+                print 'Using APASS'
+                print table.colnames
+                if 'Rmag' not in table.colnames and 'rmag' in table.colnames:
+                    table['Rmag'] = table['rmag']
+            except:
+
+                try:
+                    table = XMatch.query(cat1=open('/tmp/sourcecat.csv'),
+                                     #cat2='vizier:II/336/apass9', #apass
+                                     #cat2='vizier:I/322A/out',   # UCAC4
+                                     cat2='vizier:II/246/out',   # 2MASS
+                                     max_distance=5 * u.arcsec,
+                                     colRA1='RA',
+                                     colDec1='Dec',
+                                     colRA2='RAJ2000',
+                                     colDec2='DEJ2000')
+                    print 'Using 2MASS'
+                    print table.colnames
+                    if 'Rmag' not in table.colnames and 'rmag' in table.colnames:
+                        table['Rmag'] = table['rmag']
+                except:
+                    print('\ntable 1 avaliable?:')
+                    print XMatch.is_table_available('vizier:II/336/apass9')
+                    print('\ntable 2 avaliable?:')
+                    print XMatch.is_table_available('vizier:I/322A/out')
+                    print('\ntable 3 avaliable?:')
+                    print XMatch.is_table_available('vizier:II/246/out')
+                    raise
+    except:
+        # query PanSTARRS DR1
+        #~ catalog = panstarrs_query(crval_ra, crval_dec, 0.5, maxsources=300)
+        #~ if len(ps1_tab) < 20:
+            #~ from astroquery.vizier import Vizier
+            #~ catalog = Vizier.query_region(coord.SkyCoord(crval_ra*u.deg,
+                                                         #~ crval_dec*u.deg,
+                                                         #~ frame='icrs'),
+                                          #~ radius=coord.Angle(0.3, "deg"),
+                                          #~ catalog='UCAC4')[0]
+        #~ ra = catalog['RAJ2000']
+        #~ dec = catalog['DEJ2000']
+        #~ cat = coord.SkyCoord(ra, dec, frame='icrs')
+        #~ # cat to sources
+        #~ idx_c2s, d2d_c2s, _ = skycoords.match_to_catalog_sky(cat)
+        #~ # sources to cat
+        #~ idx_s2c, d2d_s2c, _ = cat.match_to_catalog_sky(skycoords)
+        #~ matches = cat[idx_c2s] # same length than skycoords
+        #~ idx_c2s[idx_s2c]
+        raise
+
+    from sklearn import linear_model
+    if 'UCAC4' in table.colnames:
+        table['color'] = table['Bmag'] - table['Vmag']
+    if '2MASS' in table.colnames:
+        table['color'] = table['Jmag'] - table['Kmag']
+        table['Bmag'] = table['Jmag']
 
 
+    # V Mag
+    plt.subplot(221)
+    reg = linear_model.LinearRegression()
+    sub = table[['mag_1', 'Vmag']].to_pandas()
+    sub = sub.dropna(axis=0, how='any')
+    X = sub[['mag_1']].as_matrix()
+    Y = sub['Vmag'].as_matrix()
+    reg.fit(X,Y)
+    print reg.coef_, reg.intercept_
+    regV = reg
+    table['pred_Vmag'] = reg.coef_*table['mag_1']+reg.intercept_
+    plt.plot(table['mag_1'], table['Vmag'], 'r.')
+    plt.plot(table['mag_1'], table['pred_Vmag'], 'k-', label='linear fit')
+    plt.xlabel('mag_1')
+    plt.ylabel('Vmag')
+    plt.grid()
 
-norm = ImageNormalize(stretch=SqrtStretch())
-plt.imshow(data - back, origin='lower', cmap='Greys_r', norm=norm)
-plt.show()
+    # B mag
+    plt.subplot(222)
+    reg = linear_model.LinearRegression()
+    sub = table[['mag_1', 'Bmag']].to_pandas()
+    sub = sub.dropna(axis=0, how='any')
+    X = sub[['mag_1']].as_matrix()
+    Y = sub['Bmag'].as_matrix()
+    reg.fit(X,Y)
+    print reg.coef_, reg.intercept_
+    regB = reg
+    table['pred_Bmag'] = reg.coef_*table['mag_1']+reg.intercept_
+    plt.plot(table['mag_1'], table['Bmag'], 'r.')
+    plt.plot(table['mag_1'], table['pred_Bmag'], 'k-', label='linear fit')
+    plt.xlabel('mag_1')
+    plt.ylabel('Bmag')
+    plt.grid()
 
+    # R mag
+    plt.subplot(223)
+    reg = linear_model.LinearRegression()
+    sub = table[['mag_1', 'Rmag']].to_pandas()
+    sub = sub.dropna(axis=0, how='any')
+    X = sub[['mag_1']].as_matrix()
+    Y = sub['Rmag'].as_matrix()
+    reg.fit(X,Y)
+    print reg.coef_, reg.intercept_
+    regR = reg
+    table['pred_Rmag'] = reg.coef_*table['mag_1']+reg.intercept_
+    plt.plot(table['mag_1'], table['Rmag'], 'r.')
+    plt.plot(table['mag_1'], table['pred_Rmag'], 'k-', label='linear fit')
+    plt.xlabel('mag_1')
+    plt.ylabel('Rmag')
+    plt.grid()
 
+    # Residuals vs B-V
+    plt.subplot(224)
+    plt.plot(table['color'], table['pred_Vmag']-table['Vmag'], 'g.', label='Vmag')
+    plt.plot(table['color'], table['pred_Bmag']-table['Bmag'], 'b.', label='Bmag')
+    plt.plot(table['color'], table['pred_Rmag']-table['Rmag'], 'r.', label='Rmag')
+    plt.xlabel('color')
+    plt.ylabel('Residual')
+    plt.legend(loc='best')
+    plt.grid()
 
+    plt.tight_layout()
+    plt.savefig(image_path.strip('.new')+'_calibration.png')
+    plt.clf()
 
-apertures = CircularAperture(positions, r=5.)
+    hdr['ZP_R'] = regR.intercept_
+    hdr['ZP_B'] = regB.intercept_
+    hdr['ZP_V'] = regV.intercept_
 
-norm = ImageNormalize(stretch=SqrtStretch())
-plt.imshow(data, cmap='Greys', origin='lower', norm=norm)
-apertures.plot(color='blue', lw=1.5, alpha=0.5)
+    hdr['SLOPE_R'] = regR.coef_[0]
+    hdr['SLOPE_B'] = regB.coef_[0]
+    hdr['SLOPE_V'] = regV.coef_[0]
 
+    chosen = {'band': None, 'res': 10}
+    for band in ['R', 'B', 'V']:
+        res = table['pred_'+band+'mag']-table[band+'mag']
+        residual = np.sqrt(np.sum(np.square(res)))
+        if residual < chosen['res']:
+            chosen['res'] = residual
+            chosen['band'] = band
 
+    hdr['CAL_BAND'] = chosen['band']
+    hdr['CAL_RES'] = chosen['res']
 
+    fits_file.flush()
+    fits_file.close()
 
+    band = chosen['band']
+    plt.hist(table['pred_'+band+'mag'])
+    plt.xlabel('instrumental '+band+' mag')
+    plt.savefig(image_path.strip('.new')+'_magnitudes.png')
 
+    table.write(image_path.strip('.new')+'_sources.csv', overwrite=True)
+    with open('calibrations.txt', 'a') as cals:
+        cals.write(image_path)
+        cals.write(' ')
+        cals.write('{}'.format(np.max(table['pred_'+band+'mag'])))
+        cals.write(' ')
+        cals.write('{}'.format(np.std(table['pred_'+band+'mag'])))
+        cals.write(' ')
+        cals.write('{}'.format(hdr['ZP_R']))
+        cals.write(' ')
+        cals.write('{}'.format(hdr['ZP_B']))
+        cals.write(' ')
+        cals.write('{}'.format(hdr['ZP_V']))
+        cals.write(' ')
+        cals.write('{}'.format(hdr['SLOPE_R']))
+        cals.write(' ')
+        cals.write('{}'.format(hdr['SLOPE_B']))
+        cals.write(' ')
+        cals.write('{}'.format(hdr['SLOPE_V']))
+        cals.write(' ')
+        cals.write('{}'.format(hdr['CAL_BAND']))
+        cals.write(' ')
+        cals.write('{}'.format(hdr['CAL_RES']))
+        cals.write('\n')
 
+    print 'finished'
 
-
-
-
-def main(args):
-    return 0
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+    sys.exit(cal_image(sys.argv[-1]))
